3d593644f0db1aa6fa697ed30be1f858203dd80c
Dobri Pachev <dobri.pachev@luckbox.com>
2022-08-17 11:58:30 +0300

    Optimizing

--- tests/integration-tests/repository-reader.test.ts
+++ tests/integration-tests/repository-reader.test.ts
@@ -1,5 +1,6 @@
 import ApiClient from './utils/ApiClient';
 import { expectValidationError } from './utils/expectations';
+import nock from 'nock';
 
 describe('List repositories', () => {
   const apiClient = new ApiClient();
@@ -18,10 +19,10 @@ describe('List repositories', () => {
   });
 
   it('should return HTTP status 400[3002] if username is invalid', async () => {
-    const response = await apiClient.get(endpoint, { 
+    const response = await apiClient.get(endpoint, {
       source: 'github',
       username: null,
-     }, {});
+    }, {});
 
     expectValidationError(response, [{
       code: 3002,
@@ -30,32 +31,58 @@ describe('List repositories', () => {
   });
 
   it('should return HTTP status 404[1009] if cannot determine reading client based on provided source', async () => {
-    const response = await apiClient.get(endpoint, { 
+    const response = await apiClient.get(endpoint, {
       source: 'gitlab',
       username: 'some-user',
-     }, {});
+    }, {});
 
-     expect(response.statusCode).toEqual(404);
-     expect(response.body.error.code).toEqual(1009);
+    expect(response.statusCode).toEqual(404);
+    expect(response.body.error.code).toEqual(1009);
   });
 
   it('should return HTTP status 404[1008] if user do not have any repositories', async () => {
-    const response = await apiClient.get(endpoint, { 
-      source: 'github',
-      username: 'rosenzzzz',
-     }, {});
+    const username = 'some-not-existing-username-1234';
+    nock('https://api.github.com')
+      .get(`/users/${username}?username=${username}`)
+      .reply(500);
 
-     expect(response.statusCode).toEqual(404);
-     expect(response.body.error.code).toEqual(1008);
+    const response = await apiClient.get(endpoint, {
+      source: 'github',
+      username,
+    }, {});
+
+    expect(response.statusCode).toEqual(404);
+    expect(response.body.error.code).toEqual(1008);
   });
 
   it('should return HTTP status 200 if user has need info', async () => {
-    const response = await apiClient.get(endpoint, { 
-      source: 'github',
-      username: 'Rosenberg78',
-     }, {});
+    const username = 'it-should-work-with-this-user';
+    nock('https://api.github.com')
+      .persist()
+      .get(`/users/${username}?username=${username}`)
+      .reply(200, { public_repos: 1 })
+      .get(`/users/${username}/repos?username=${username}&per_page=30&page=1`)
+      .reply(200, [{
+        fork: false,
+        name: 'project-name-some2',
+        owner: {
+          login: username,
+        },
+      }])
+      .get(`/repos/${username}/project-name-some2/branches?owner=${username}&repo=project-name-some2`)
+      .reply(200, [{
+        name: 'master',
+        commit: {
+          sha: '10c106b62b32be48b950e3b7375d5fd1cc7260c7',
+        },
+      }]);
 
-     expect(response.statusCode).toEqual(200);
+    const response = await apiClient.get(endpoint, {
+      source: 'github',
+      username,
+    }, {});
+
+    expect(response.statusCode).toEqual(200);
   });
   
 });
--- tests/unit-tests/queries/FetchUserReposQuery.test.ts
+++ tests/unit-tests/queries/FetchUserReposQuery.test.ts
@@ -1,27 +1,23 @@
 import UserDoNotExistError from '@application/errors/UserDoNotExistError';
 import UserReposDoNotExistError from '@application/errors/UserReposDoNotExistError';
 import FetchUserReposQuery from '@application/queries/FetchUserReposQuery';
-import ReaderClientResolver from '@application/services/ReaderClientResolver';
 import { makeNonEmptyString } from '@domain/types';
-import GitHubReaderClient from '@infrastructure/gitHub/GitHubReaderClient';
-import MockReplicator from '@tests/util/MockReplicator';
+import container from '../../../root';
 
 describe('FetchUserReposQuery', () => {
-  const mockReplicator = new MockReplicator();
-  const mockedReaderClient = mockReplicator.replicate(GitHubReaderClient);
-  const resolver =  new ReaderClientResolver([mockedReaderClient]);
-
-  (mockedReaderClient.vendor as string) = 'github';
+  const githubReader = container.resolve('readerClients')[0];
+  const resolver = container.resolve('readerClientResolver');
+  
   const query = new FetchUserReposQuery(
     resolver,
   );
 
-  afterEach(() => {
-    mockedReaderClient.mockClear();
+  beforeEach(() => {
+    jest.clearAllMocks();
   });
 
   it('should throw UserDoNotExistError if user with that name do not exist in the vendor system that is used for extracting repos', async () => {
-    mockedReaderClient.extractHowManyReposDoesTheUserHave.mockReturnValueOnce({ data: { public_repos: 0 } });
+    jest.spyOn(githubReader, 'extractHowManyReposDoesTheUserHave').mockReturnValueOnce({ data: { public_repos: 0 } });
 
     await expect(query.fetch({
       username: makeNonEmptyString('Someone'),
@@ -30,45 +26,65 @@ describe('FetchUserReposQuery', () => {
   });
 
   it('should throw UserReposDoNotExistError if user with that name do not exist in the vendor system that is used for extracting repos', async () => {
-    mockedReaderClient.extractHowManyReposDoesTheUserHave.mockReturnValueOnce({ data: {
-      public_repos: 1,
-    } });
-
-    mockedReaderClient.extractReposPerUser.mockReturnValueOnce([{ data: null }]);
-
+    jest.spyOn(githubReader, 'extractHowManyReposDoesTheUserHave').mockReturnValueOnce({ data: { public_repos: 1 } });
+    jest.spyOn(githubReader, 'extractReposPerUser').mockReturnValueOnce({ data: null });
     await expect(query.fetch({
       username: makeNonEmptyString('Someone'),
       source: makeNonEmptyString('github'),
     })).rejects.toThrow(UserReposDoNotExistError);
   });
 
-  it('should return number of repos and filtered repos', async () => {
-    mockedReaderClient.extractHowManyReposDoesTheUserHave.mockReturnValueOnce({ data: {
-      public_repos: 1,
-    } });
-
-    mockedReaderClient.extractReposPerUser.mockReturnValueOnce({
-      data: [{
-        fork: false,
-        name: 'project-name-some2',
-        owner: {
-          login: 'Someone',
-        },
-      }],
+  it('should return number of repos for specified user', async () => {
+    jest.spyOn(githubReader, 'extractHowManyReposDoesTheUserHave').mockReturnValueOnce({ data: { public_repos: 1 } });
+   
+    const result = await query.fetch({
+      username: makeNonEmptyString('Someone'),
+      source: makeNonEmptyString('github'),
     });
+
+    expect(result.count).toEqual(1);
+  });
+
+  it('should return empty array if user repos are all forks', async () => {
+    jest.spyOn(githubReader, 'extractHowManyReposDoesTheUserHave').mockReturnValueOnce({ data: { public_repos: 1 } });
+    jest.spyOn(githubReader, 'extractReposPerUser').mockReturnValueOnce({ data: [{
+      fork: true,
+      name: 'project-name-some2',
+      owner: {
+        login: 'Someone',
+      },
+    }] });
+
+    const result = await query.fetch({
+      username: makeNonEmptyString('Someone'),
+      source: makeNonEmptyString('github'),
+    });
+
+    expect(result.count).toEqual(1);
+    expect(result.items).toBeArrayOfSize(0);
+  });
+
+  it('should return number of repos and filtered (non forked ones) repos', async () => {
+    jest.spyOn(githubReader, 'extractHowManyReposDoesTheUserHave').mockReturnValueOnce({ data: { public_repos: 1 } });
+    jest.spyOn(githubReader, 'extractReposPerUser').mockReturnValueOnce({ data: [{
+      fork: false,
+      name: 'project-name-some2',
+      owner: {
+        login: 'Someone',
+      },
+    }] });
     
     const reposPerUser = [
       {
         name: 'master',
         commit: {
           sha: '10c106b62b32be48b950e3b7375d5fd1cc7260c7',
-          url: 'https://api.github.com/repos/Someone/project-name-some2/commits/10c106b62b32be48b950e3b7375d5fd1cc7260c7'
+          url: 'https://api.github.com/repos/Someone/project-name-some2/commits/10c106b62b32be48b950e3b7375d5fd1cc7260c7',
         },
       },
-    ]
-    mockedReaderClient.extractBranchesPerRepo.mockReturnValueOnce({
-      data: reposPerUser,
-    })
+    ];
+
+    jest.spyOn(githubReader, 'extractBranchesPerRepo').mockReturnValueOnce({ data: reposPerUser });
 
     const result = await query.fetch({
       username: makeNonEmptyString('Someone'),
@@ -77,13 +93,12 @@ describe('FetchUserReposQuery', () => {
 
     expect(result.count).toEqual(1);
     expect(result.items[0]).toEqual({
-      name: "project-name-some2",
-      ownerLogin: "Someone",
+      name: 'project-name-some2',
+      ownerLogin: 'Someone',
       branches: [{
-        branchName: "master",
-        commitSha: "10c106b62b32be48b950e3b7375d5fd1cc7260c7"
-      }]
-    })
+        branchName: 'master',
+        commitSha: '10c106b62b32be48b950e3b7375d5fd1cc7260c7',
+      }],
+    });
   });
-
 });
